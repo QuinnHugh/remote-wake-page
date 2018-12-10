482de08f827daf8da63189897271cec42b5c0a31
Kun <quinnHugh@hotmail.com>
2018-12-10 13:42:56 +0800

    update wol index and server

--- wol-web-server.go
+++ wol-web-server.go
@@ -15,17 +15,16 @@ func sender(conn net.Conn , macaddr string) bool {
 
 
 
-func wol(who string) bool {
+func wol(magicword string) bool {
 	var macaddr string
 	statue := false
-	switch who {
-	case "/449158ec31537019": macaddr = "0c9d92bd7d15"//"kun"
-	case "/c9a2599317183c9c": macaddr = "0c9d92bdfa36"//"chengxiang"
-	default: macaddr = ""	
+	switch magicword {
+	case "open kun": macaddr = "0c9d92bd7d15"
+	case "chengxiang": macaddr = "0c9d92bdfa36"
+	default : return statue
+		
 	}
-	if macaddr==""{
-		return statue
-	}
+	
 	server := "yumn.tk:777"
 	//server := "127.0.0.1:8001"
 	tcpAddr, err := net.ResolveTCPAddr("tcp4", server)
@@ -45,44 +44,50 @@ func wol(who string) bool {
 	return statue
 }
 
-func webResponse(w http.ResponseWriter, r *http.Request)  {
-	r.ParseForm()  //解析参数，默认是不会解析的
-	//fmt.Println(r.Form)  //这些信息是输出到服务器端的打印信息
-	wolpara := r.URL.Path
-	fmt.Println("path", wolpara)
-	//fmt.Println("scheme", r.URL.Scheme)
-	//fmt.Println(r.Form["url_long"])
-	statue := wol(wolpara)
-	if !statue{
-		fmt.Fprintf(w,"失败")
-		return	
-	} else {
-		fmt.Fprintf(w,"成功!")
-		return
-	}	
-}
+// func webResponse(w http.ResponseWriter, r *http.Request)  {
+// 	r.ParseForm()  //解析参数，默认是不会解析的
+// 	//fmt.Println(r.Form)  //这些信息是输出到服务器端的打印信息
+// 	wolpara := r.URL.Path
+// 	fmt.Println("path", wolpara)
+// 	//fmt.Println("scheme", r.URL.Scheme)
+// 	//fmt.Println(r.Form["url_long"])
+    
+	
+// 	statue := wol(wolpara)
+// 	if !statue{
+// 		fmt.Fprintf(w,"失败")
+// 		return	
+// 	} else {
+// 		fmt.Fprintf(w,"成功!")
+// 		return
+// 	}	
+// }
 
 func submitResponse(w http.ResponseWriter, r *http.Request){
-	fmt.Fprintf(w,"this is the page")
+	statue := false
+	fmt.Fprintf(w,"Parsing the form data...\n")
 	r.ParseForm()  //解析参数，默认是不会解析的
-	fmt.Println(w, r.Form)  //这些信息是输出到服务器端的打印信息
-	wolpara := r.URL.Path
-	fmt.Println("path", wolpara)
+	//fmt.Println(w, r.Form)  //这些信息是输出到服务器端的打印信息
+	fmt.Fprintf(w,"Sanding waking command...\n")
+	wolpara := r.PostFormValue("magic word")
+	//fmt.Println(wolpara)
 	//fmt.Println("scheme", r.URL.Scheme)
-	//fmt.Println(r.Form["url_long"])
-	// statue := wol(wolpara)
-	// if !statue{
-	// 	fmt.Fprintf(w,"失败")
-	// 	return	
-	// }else{
-	// 	fmt.Fprintf(w,"成功!")
-	// 	return
-	// }
+	if wolpara != "Your Magic Word"{
+		statue = wol(wolpara)
+	}else{
+		fmt.Fprintf(w,"Type in your magic word please\n")
+	}
+	if !statue{
+		fmt.Fprintf(w,"Failed!\n")
+		return	
+	}else{
+		fmt.Fprintf(w,"Succeed!\n")
+		return
+	}
 }
 
 func main() {
-	fmt.Println("1")
-	http.HandleFunc("/",webResponse)
+	//http.HandleFunc("/",webResponse)
 	http.HandleFunc("/da1e3053f72d38d8",submitResponse)
 
     err := http.ListenAndServe(":9000",nil)
